607576c39603bb8581cadc7327b9ff28a18578cf
Natalia T <natalia.torres@umain.com>
2025-09-23 14:42:18 +0200

    main styling for all cards done

---
+++ src/app/components/FilterByFood.jsx
@@ -0,0 +1,28 @@
+export default function FilterByFood() {
+  return ( 
+
+<section className="flex  gap-2.5 items-center">
+          <div className="card-style flex gap-2 h-15">
+            <h3 className="pt-4 px-3">Hamburgers</h3>
+            <p className="pt-5">Image</p>
+          </div>
+            <div className="card-style flex gap-2 h-15">
+            <h3 className="pt-4 px-3">Pizza</h3>
+            <p className="pt-5">Image</p>
+          </div>
+            <div className="card-style flex gap-2 h-15">
+            <h3 className="pt-4 px-3">Tacos</h3>
+            <p className="pt-5">Image</p>
+          </div>
+            <div className="card-style flex gap-2 h-15">
+            <h3 className="pt-4 px-3">Coffee</h3>
+            <p className="pt-5">Image</p>
+          </div>
+            <div className="card-style flex gap-2 h-15">
+            <h3 className="pt-4 px-3">Fries</h3>
+            <p className="pt-5">Image</p>
+          </div>
+       
+        </section>
+
+   )}
---
+++ src/app/components/FilterNavBar.jsx
@@ -0,0 +1,33 @@
+export default function FilterNavBar() {
+  return (  
+<nav className="w-1/6 card-style px-6 flex flex-col min-w-36 w-60">
+          <h2 className="pt-4 text-2xl ">Filter</h2>
+
+          <h3 className="text-xs text-gray-400 tex-bold mt-8 mb-4">FOOD CATEGORY</h3>
+          <ul className="flex flex-col gap-2.5 ">
+
+              <button className="button-style">Hamburger</button>
+              <button className="button-style">Pizza</button>
+              <button className="button-style">Tacos</button>
+              <button className="button-style">Coffee</button>
+          </ul>
+              <h3 className="text-xs text-gray-400 tex-bold mt-8 mb-4">DELIVERY TIME</h3>
+          <ul className="flex flex-wrap gap-2.5">
+            
+              <button className="button-style">0-10 min</button>
+              <button className="button-style">10-30 min</button>
+              <button className="button-style">30-60 min</button>
+              <button className="button-style">1 hour+</button>
+          </ul>
+             <h3 className="text-xs text-gray-400 tex-bold mt-8 mb-4">PRICE RANGE</h3>
+            <ul className="flex flex-wrap gap-2.5 ">
+            
+              <button className="button-style">$</button>
+              <button className="button-style">$$</button>
+              <button className="button-style">$$$</button>
+              <button className="button-style">$$$$</button>
+    
+          </ul>
+      
+        </nav>
+  )}
