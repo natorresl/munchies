01a857b516a128143b47997629771951af0d49ef
Natalia T <natalia.torres@umain.com>
2025-09-24 15:42:40 +0200

    filter categories map

--- src/app/components/FilterFoodCategory.jsx
+++ src/app/components/FilterFoodCategory.jsx
@@ -11,16 +11,14 @@ export default async function FilterFoodCategory() {
   return ( 
 
 <section className="flex h-20 min-h-20 w-full gap-2.5 items-center overflow-x-auto  text-sm">
-    <ul>
+    <ul className="flex h-20 min-h-20 w-full gap-2.5 items-center overflow-x-auto  text-sm">
       {filters.map((filter) => (
-        <li key={filter.id}>{filter.name}</li>
-      ))}
-    </ul>
-
-      <div className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative">
-            <h3 className="pt-4 pl-3">Hamburgers</h3>
+        <button key={filter.id}
+        className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative cursor-pointer"
+        >
+         <h3 className="pt-4 pl-3">{filter.name}</h3>
             <Image
-              src="/images/hamburger.png"
+              src={`${filter.image_url}`}
               alt="Hamburger Icon"
               width={80}
               height={80}
@@ -28,74 +26,9 @@ export default async function FilterFoodCategory() {
            
         
             />
-          </div>
-          <div className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative">
-            <h3 className="pt-4 pl-3">Pizza</h3>
-              <Image
-              src="/images/pizza.png"
-              alt="Pizza Icon"
-              width={80}
-              height={80}
-              className="overflow-hidden absolute left-22"
-        
-            />
-          </div>
-        <div className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative">
-            <h3 className="pt-4 px-3">Tacos</h3>
-             <Image
-              src="/images/taco.png"
-              alt="Taco Icon"
-              width={80}
-              height={80}
-                className="overflow-hidden absolute left-22"
-        
-            />
-          </div>
-          <div className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative">
-            <h3 className="pt-4 px-3">Coffee</h3>
-                <Image
-              src="/images/coffee.png"
-              alt="Coffee Icon"
-              width={80}
-              height={80}
-                className="overflow-hidden absolute left-22"
-        
-            />
-          </div>
-         <div className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative">
-            <h3 className="pt-4 px-3">Fries</h3>
-                 <Image
-              src="/images/Fries.png"
-              alt="Fries Icon"
-              width={80}
-              height={80}
-                className="overflow-hidden absolute left-22"
-        
-            />
-          </div>
-             <div className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative">
-            <h3 className="pt-4 px-3">Mexican</h3>
-                 <Image
-              src="/images/burrito.png"
-              alt="Egg and Bacon Icon"
-              width={80}
-              height={80}
-                className="overflow-hidden absolute left-22"
-        
-            />
-          </div>
-         <div className="card-style min-w-40 w-40 h-full flex gap-2 justify-between relative">
-            <h3 className="pt-4 px-3">Breakfast</h3>
-                 <Image
-              src="/images/breakfast.png"
-              alt="Egg and Bacon Icon"
-              width={80}
-              height={80}
-                className="overflow-hidden absolute left-22"
-        
-            />
-          </div>
-       
+        </button>
+      ))}
+    </ul>  
         </section>
 
    )}
