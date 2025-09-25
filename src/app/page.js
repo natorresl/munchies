dae02a80c7e4681bb4eddbc20c03cb3dddfc85f2
Natalia T <natalia.torres@umain.com>
2025-09-25 13:33:59 +0200

    fetch all data for FilterNavBar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,30 @@
+"use client"
+
 import Image from "next/image";
 import FilterNavBar from "./components/FilterNavBar";
 import FilterFoodCategory from "./components/FilterFoodCategory";
 import RestaurantCards from "./components/RestaurantCards";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+async function fetchData() {  
+
+  const dataRestaurants = await fetch('https://work-test-web-2024-eze6j4scpq-lz.a.run.app/api/restaurants')
+    const restaurantJson = await dataRestaurants.json()
+    setRestaurants(restaurantJson);
+
+}
+fetchData();
+  }, []);
+  console.log(restaurants);
+     
+
+
   return (
- <div className="min-h-screen h-screen py-10 pl-10 sm:py-10 sm:pl-10
+ <div className="min-h-screen h-screen py-10 pl-10 sm:py-10 sm:pl-10 max-w-[1440px]
                     items-center ">
                       <Image
           src="/images/munchies-logo.png"
